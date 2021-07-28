b053c6eb38a71c26b8d98bec00ca1d9a5494e603
AndhyGT <andhyralac11@gmail.com>
2021-07-27 21:42:00 -0600

    agregando archivos

---
+++ README.md
@@ -0,0 +1,11 @@
+## CLON SENCILLO PINTEREST
+
+### clon sencillo de pinterest para seguir aprendiendo la subida de imagenes
+### y como renderizarlo en el navegador
+
+#### Algunas tecnologias utilizadas:
+#### Node.js
+#### Framework Express.js
+#### Conexion a base de datos MongoDB con Moongose
+#### Multer.js
+
---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const multer = require('multer');
+const { v4 } = require('uuid');
+const { format } = require('timeago.js');
+
+// Initializations
+const app = express();
+require('./database');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/img/uploads'),
+    filename: (req, file, cb, filename) => {
+        cb(null, v4() + path.extname(file.originalname));
+    }
+});
+app.use(multer({
+    storage
+}).single('image'));
+
+
+// Global variables
+app.use((req, res, next) => {
+    app.locals.format = format;
+    next();
+})
+
+
+// Routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// star the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
